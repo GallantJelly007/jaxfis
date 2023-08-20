c086a7327b8083c8e455c2a2bc9cca321fd2318b
Alex <alariel007@gmail.com>
2023-08-21 00:25:30 +0600

    add export url class

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import Jax from "./jax.mjs";
-import { JBuffer, JFormData, JFile, JFileList } from "./classes.mjs";
+import { JBuffer, JFormData, JFile, JFileList, Url } from "./classes.mjs";
 
 export{
     JBuffer,
     JFormData,
     JFile,
     JFileList,
+    Url,
     Jax
 }
